717d0349ef093ddd1214a8b514d3e6ef1ece7b3e
Mikko Johansson <mikko.johansson2@gmail.com>
2020-01-29 07:51:17 +0200

    Added apartment class

---
+++ RealEstateProject/src/main/java/fi/amiedu/realestateproject/domain/Apartment.java
@@ -0,0 +1,69 @@
+package fi.amiedu.realestateproject.domain;
+
+import java.util.List;
+
+public class Apartment extends Property {
+
+	private int floor;
+	private int totalFloors;
+	private boolean elevator;
+	private boolean balcony;
+	
+	public Apartment(Lot lot, Address address, List<Picture> pictures, Picture floorPlan, String prize, float area,
+			String desc, int floor, int totalFloors, boolean elevator, boolean balcony) {
+		super(lot, address, pictures, floorPlan, prize, area, desc);
+		this.floor = floor;
+		this.totalFloors = totalFloors;
+		this.elevator = elevator;
+		this.balcony = balcony;
+	}
+
+	public int getFloor() {
+		return floor;
+	}
+
+	public void setFloor(int floor) {
+		this.floor = floor;
+	}
+
+	public int getTotalFloors() {
+		return totalFloors;
+	}
+
+	public void setTotalFloors(int totalFloors) {
+		this.totalFloors = totalFloors;
+	}
+
+	public boolean isElevator() {
+		return elevator;
+	}
+
+	public void setElevator(boolean elevator) {
+		this.elevator = elevator;
+	}
+
+	public boolean isBalcony() {
+		return balcony;
+	}
+
+	public void setBalcony(boolean balcony) {
+		this.balcony = balcony;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(super.toString());
+		builder.append("Apartment [floor=");
+		builder.append(floor);
+		builder.append(", totalFloors=");
+		builder.append(totalFloors);
+		builder.append(", elevator=");
+		builder.append(elevator);
+		builder.append(", balcony=");
+		builder.append(balcony);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
