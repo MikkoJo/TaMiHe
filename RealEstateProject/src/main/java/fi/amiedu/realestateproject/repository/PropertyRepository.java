5c1942c6b60181a5c80052174526f1fc3c8410e4
Mikko Johansson <mikko.johansson2@gmail.com>
2020-01-30 15:17:42 +0200

    Added some query methods

--- RealEstateProject/src/main/java/fi/amiedu/realestateproject/repository/PropertyRepository.java
+++ RealEstateProject/src/main/java/fi/amiedu/realestateproject/repository/PropertyRepository.java
@@ -1,9 +1,17 @@
 package fi.amiedu.realestateproject.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
+import fi.amiedu.realestateproject.domain.Apartment;
 import fi.amiedu.realestateproject.domain.Property;
 
 public interface PropertyRepository extends CrudRepository<Property, Integer>{
 
+	List<Property> findByAddressCountry(String country);
+	List<Property> findByAddressStreetAddressContaining(String streetAddress);
+	List<Property> findByAddressCity(String city);
+	List<Property> findByPriceLessThanEqual(double price);
+	List<Apartment> findByBalconyTrue();
 }
