2d6c2ebeb228f8af9ab52a132a9f6788a242b597
Mikko Johansson <mikko.johansson2@gmail.com>
2020-02-13 11:28:06 +0200

    Added address search

--- real_est_proj_ui/src/App.js
+++ real_est_proj_ui/src/App.js
@@ -6,8 +6,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       apartments: [],
-      address: '',
-      name: ''
+      address: ''
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -16,65 +15,61 @@ class App extends React.Component {
   }
 
   handleChange(event) {
-    // console.log(event.currentTarget);
-    // console.log(event.target.id);
-    // console.log(event.target.value);
-    if(event.target.id === 'address') {
-      this.setState({address: event.target.value});
+    if (event.target.id === 'address') {
+      this.setState({ address: event.target.value });
     }
-    }
-    handleSubmit(event) {
-      // alert('Your favorite flavor is: ' + this.state.value);
-      event.preventDefault();
-      // console.log(event);
-      // this.getValue('name');
-      console.log(this.state.langs);
-//      const addr = new FormData(event.target);
-      fetch("http://localhost:8080/apartment?address=" + this.state.address)
-        .then(res => res.json()
-        .then((data => {
-          this.setState({ apartments: data })
-        })
-        )
-        .catch(console.log)
-        )}
+  }
   
-
-    componentDidMount() {
-      fetch("http://localhost:8080/apartment")
-        .then(res => res.json()
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.langs);
+    fetch("http://localhost:8080/apartment?address=" + this.state.address)
+      .then(res => res.json()
         .then((data => {
           this.setState({ apartments: data })
         })
         )
         .catch(console.log)
-        )}
+      )
+  }
+
+
+  componentDidMount() {
+    fetch("http://localhost:8080/apartment")
+      .then(res => res.json()
+        .then((data => {
+          this.setState({ apartments: data })
+        })
+        )
+        .catch(console.log)
+      )
+  }
 
   render() {
-  return (
-    <div>
-      <h1 className="App">Kiinteistöesittely</h1>
-    
-    <div>    
-      <ul>
-       {this.state.apartments.map(function(apar) {
-         return <li key={apar.id}>
-          <div>{apar.address.streetAddress} {apar.address.zipCode} {apar.address.city}</div>
-          {apar.pictures.map(function(pic) {
-            return <img key={pic.id} src={pic.url} alt="" style={{width:'300px', margin:'10px'}} />
+    return (
+      <div>
+        <h1 className="App">Kiinteistöesittely</h1>
 
-          })}
-         </li>
-       })} 
-      </ul>
-    </div>
-    <form id='filter' onSubmit={this.handleSubmit}>
-            <label htmlFor='address'>Osoite:</label>
-            <input id='address' name='address' type='text' onChange={this.handleChange}></input><br />
-            <button type='submit'>Hae Osoitteella</button>
-          </form>
-    </div>
-  );
+        <div>
+          <ul>
+            {this.state.apartments.map(function (apar) {
+              return <li key={apar.id}>
+                <div>{apar.address.streetAddress} {apar.address.zipCode} {apar.address.city}</div>
+                {apar.pictures.map(function (pic) {
+                  return <img key={pic.id} src={pic.url} alt="" style={{ width: '300px', margin: '10px' }} />
+
+                })}
+              </li>
+            })}
+          </ul>
+        </div>
+        <form id='filter' onSubmit={this.handleSubmit}>
+          <label htmlFor='address'>Osoite:</label>
+          <input id='address' name='address' type='text' onChange={this.handleChange}></input><br />
+          <button type='submit'>Hae Osoitteella</button>
+        </form>
+      </div>
+    );
   }
 }
 
